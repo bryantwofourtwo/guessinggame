02573315010c58e764bb7bce16b59101b2e38266
bryantwofourtwo <bryantwofourtwo@gmail.com>
2018-05-16 15:38:45 -0600

    a little number guessing game

---
+++ game.js
@@ -0,0 +1,21 @@
+
+console.log("Hi there!");
+// create secretb number
+var secretNum = 4;
+
+// ask user for guess
+var stringGuess = prompt("Guess a number");
+var guess = Number(stringGuess);
+// check if guess is correct
+if(guess === secretNum) {
+    console.log("You are correct!!!!");
+}
+// otherwise check if higher
+else if(guess > secretNum) {
+    console.log("Too much number, must less");
+}  
+// otherwise check if lower
+else {
+    console.log("You too small, you must embiggen your number");
+}
+
