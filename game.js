c93a9b3de5ed4fd5a1d7aa87da95f3245cb6118f
bryantwofourtwo <bryantwofourtwo@gmail.com>
2018-05-24 10:51:06 -0600

    salad for days

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
 
 console.log("Hi there!");
 // create secretb number
-var secretNums = 4;
+var secretNum = 4;
 
 // ask user for guess
 var stringGuess = prompt("Guess a number");
 var guess = Number(stringGuess);
 // check if guess is correct
-if(guess === secretNums) {
+if(guess === secretNum) {
     console.log("You are correct!!!!");
 }
 // otherwise check if higher
-else if(guess > secretNums) {
+else if(guess > secretNum) {
     console.log("Too much number, must less more");
 }  
 // otherwise check if lower
